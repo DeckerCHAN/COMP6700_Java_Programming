096d92cc5638196ae8017d8e51351dd9253acede
DeckerCHAN <DeckerCHAN@gmail.com>
2016-03-02 12:14:08 +1100

    Format outputs.

--- src/main/java/com/decker/javaProgramming/homework/hw2/EulerBernoulliTriangle.java
+++ src/main/java/com/decker/javaProgramming/homework/hw2/EulerBernoulliTriangle.java
@@ -29,33 +29,52 @@ import java.math.BigInteger;
 
 public class EulerBernoulliTriangle {
     private int height;
-    private long[][] elems;
+    private BigInteger[][] elems;
 
     public EulerBernoulliTriangle(int h) {
         this.height = h;
         initialize();
         //printTriangle();
-        System.out.println("The Euler numbers:\n" + getEulerNumbers());
-        System.out.println("The Bernoulli numbers:\n" + getBernoulliNumbers());
+        //System.out.println("The Euler numbers:\n" + getEulerNumbers());
+        this.printEulerTriangle();
+        //System.out.println("The Bernoulli numbers:\n" + getBernoulliNumbers());
+    }
+
+    public static void main(String[] args) {
+        assert args.length > 0;
+        int i = 0;
+        try {
+            i = Integer.parseInt(args[0]);
+        } catch (NumberFormatException nfe) {
+            System.out.println(nfe.toString() + " must be odd integer");
+        } finally {
+            if (i % 2 != 1 || i == 1) {
+                System.out.println("Enter an odd number greater than 2 only");
+                System.exit(1);
+            }
+            System.out.println("The height of the triangle is " + i);
+        }
+
+        EulerBernoulliTriangle triangle = new EulerBernoulliTriangle(i);
     }
 
     private void initialize() {
-        elems = new long[height][];
-        elems[0] = new long[1];
-        elems[0][0] = 1;
+        elems = new BigInteger[height][];
+        elems[0] = new BigInteger[1];
+        elems[0][0] = BigInteger.valueOf(1);
         int lodd, leven;
-        for (int i = 1; i < height; i+=2) {
-            lodd = i+1;
-            leven = i+2;
-            elems[i] = new long[lodd];
-            elems[i+1] = new long[leven];
-            elems[i][0] = 0;
-            elems[i+1][leven - 1] = 0;
+        for (int i = 1; i < height; i += 2) {
+            lodd = i + 1;
+            leven = i + 2;
+            elems[i] = new BigInteger[lodd];
+            elems[i + 1] = new BigInteger[leven];
+            elems[i][0] = BigInteger.valueOf(0);
+            elems[i + 1][leven - 1] = BigInteger.valueOf(0);
             for (int j = 1; j < lodd; j++) {
-                elems[i][j] = elems[i-1][j-1] + elems[i][j-1];
+                elems[i][j] = elems[i - 1][j - 1].add(elems[i][j - 1]);
             }
             for (int j = leven - 2; j >= 0; j--) {
-                elems[i+1][j] = elems[i+1][j+1] + elems[i][j];
+                elems[i + 1][j] = elems[i + 1][j + 1].add(elems[i][j]);
             }
         }
     }
@@ -68,51 +87,46 @@ public class EulerBernoulliTriangle {
         }
     }
 
-    public long[] euler() {
-        long[] left = new long[(height-1) / 2];
-        left[0] = 1;
-        for (int i = 1; i<left.length; i++)
-            left[i] = elems[2*i][0];
+    public BigInteger[] euler() {
+        BigInteger[] left = new BigInteger[(height - 1) / 2];
+        left[0] = BigInteger.valueOf(1);
+        for (int i = 1; i < left.length; i++)
+            left[i] = elems[2 * i][0];
         return left;
     }
 
     public String getEulerNumbers() {
         String s = "";
-        for(Long eul : euler())
+        for (BigInteger eul : euler())
             s += eul + "\n";
-        return s + "That\'s " + euler().length + " Euler numbers\n" ;
+        return s + "That\'s " + euler().length + " Euler numbers\n";
     }
 
-    public long[] bernoulli() {
-        long[] right = new long[height / 2];
-        right[0] = 1;
-        for (int i = 1; i<right.length; i++)
-            right[i] = elems[2*i - 1][2*i - 1];
+    public void printEulerTriangle() {
+        System.out.println(" n                                                               Ex");
+        System.out.println("-------------------------------------------------------------------");
+
+        BigInteger[] euler = euler();
+        for (int i = 1, eulerLength = euler.length; i < eulerLength; i++) {
+            BigInteger eul = euler[i];
+            System.out.printf("%2d%65s%n",i,eul.toString());
+
+        }
+
+    }
+
+    public BigInteger[] bernoulli() {
+        BigInteger[] right = new BigInteger[height / 2];
+        right[0] = BigInteger.valueOf(1);
+        for (int i = 1; i < right.length; i++)
+            right[i] = elems[2 * i - 1][2 * i - 1];
         return right;
     }
 
     public String getBernoulliNumbers() {
         String s = "";
-        for(Long ber : bernoulli())
+        for (BigInteger ber : bernoulli())
             s += ber + "\n";
-        return s + "That\'s " + bernoulli().length + " Bernoulli numbers\n" ;
-    }
-
-    public static void main(String[] args) {
-        assert args.length > 0;
-        int i = 0;
-        try {
-            i = Integer.parseInt(args[0]);
-        } catch (NumberFormatException nfe) {
-            System.out.println(nfe.toString() + " must be odd integer");
-        } finally {
-            if (i%2 != 1 || i == 1) {
-                System.out.println("Enter an odd number greater than 2 only");
-                System.exit(1);
-            }
-            System.out.println("The height of the triangle is " + i);
-        }
-
-        EulerBernoulliTriangle triangle = new EulerBernoulliTriangle(i);
+        return s + "That\'s " + bernoulli().length + " Bernoulli numbers\n";
     }
 }
