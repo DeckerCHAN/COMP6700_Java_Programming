0c8d5b8a629c3068890914c86afacde9a1bb3a10
DeckerCHAN <DeckerCHAN@gmail.com>
2016-03-16 12:02:21 +1100

    Added newline.

--- src/main/java/com/decker/javaProgramming/homework/hw3/Factors.java
+++ src/main/java/com/decker/javaProgramming/homework/hw3/Factors.java
@@ -20,6 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
  */
 
 package com.decker.javaProgramming.homework.hw3;
@@ -49,5 +50,6 @@ public class Factors {
         if (isPrimeNumber) {
             System.out.printf("%n(%d is a prime number)", orig);
         }
+        System.out.println();
     }
 }
